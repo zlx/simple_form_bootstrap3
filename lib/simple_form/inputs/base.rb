heartcombo/simple_form

make all old test pass

lib/simple_form/inputs/base.rb
@@ -69,7 +69,11 @@ module SimpleForm
           input_html_classes << SimpleForm.input_class
         end
 
-        @input_html_options = html_options_for(:input, input_html_classes << bootstrap3_class).tap do |o|
+        if SimpleForm.default_wrapper == :bootstrap3
+          input_html_classes << bootstrap3_clas
+        end
+
+        @input_html_options = html_options_for(:input, input_html_classes).tap do |o|
           o[:readonly]  = true if has_readonly?
           o[:disabled]  = true if has_disabled?
           o[:autofocus] = true if has_autofocus?
@@ -182,7 +186,6 @@ module SimpleForm
         I18n.t(lookups.shift, scope: :"simple_form.#{namespace}", default: lookups).presence
       end
 
-      # FIXME add when use bootstrap3 wrappers
       def bootstrap3_class
         "form-control"
       end

zlx/simple_form_bootstrap3

lib/simple_form/inputs/base.rb
@@ -65,7 +65,10 @@ module SimpleForm
         @html_classes = SimpleForm.additional_classes_for(:input) { additional_classes }
 
         @input_html_classes = @html_classes.dup
-        @input_html_options = html_options_for(:input, input_html_classes << bootstrap3_class).tap do |o|
+        if SimpleForm.default_wrapper == :bootstrap3
+          input_html_classes << bootstrap3_class
+        end
+        @input_html_options = html_options_for(:input, input_html_classes).tap do |o|
           o[:readonly]  = true if has_readonly?
           o[:disabled]  = true if has_disabled?
           o[:autofocus] = true if has_autofocus?
@@ -89,7 +92,6 @@ module SimpleForm
       end
 
       private
-      # FIXME add when use bootstrap3 wrappers
       def bootstrap3_class
        "form-control"
       end